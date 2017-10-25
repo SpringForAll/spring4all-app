a8dc401fc2cd6724777c99c1e690afdf37c3ea7c
yanghuiqiang <yanghuiqiang@meituan.com>
2017-10-25 20:19:03 +0800

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {IonicPage, NavController, NavParams, Slides} from 'ionic-angular';
 import {TranslateService} from "@ngx-translate/core";
+import {Api} from "../../providers/api/api"
 
 /**
  * Generated class for the HomePage page.
@@ -21,7 +22,7 @@ export class HomePage {
   slideData: Array<any> = [];
   pages: Array<any> = [];
 
-  constructor(translateService: TranslateService, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(translateService: TranslateService, public navCtrl: NavController, public navParams: NavParams,public api:Api) {
     translateService.get('TAB1_TITLE').subscribe(title => {
       this.title = title;
     });
@@ -42,90 +43,38 @@ export class HomePage {
   }
 
   getPages() {
-    this.pages = [
-      {
-        "title": "MySQL Schema与数据类型的优化",
-        "abstract": "选择优化的数据类型： 1、 更小的通常更好： 一般情况下，应该尽量使用可以正确存储数据的最小数据类型。",
-        "time": "杨小强 发布于 4小时前",
-        "like": "12",
-        "commit": "21"
-      },
-      {
-        "title": "疯狂Spring Cloud连载（十一）——Feign的编码器与解码器",
-        "abstract": " 本文节选自《疯狂Spring Cloud微服务架构实战》，本书将于2017年11月出版。         Spring Cloud教学视频：",
-        "time": "杨小强 发布于 一年前",
-        "like": "122",
-        "commit": "101"
-      },
-      {
-        "title": "MySQL Schema与数据类型的优化",
-        "abstract": "选择优化的数据类型： 1、 更小的通常更好： 一般情况下，应该尽量使用可以正确存储数据的最小数据类型。",
-        "time": "杨小强 发布于 4小时前",
-        "like": "12",
-        "commit": "21"
-      },
-      {
-        "title": "MySQL Schema与数据类型的优化",
-        "abstract": "选择优化的数据类型： 1、 更小的通常更好： 一般情况下，应该尽量使用可以正确存储数据的最小数据类型。",
-        "time": "杨小强 发布于 4小时前",
-        "like": "12",
-        "commit": "21"
-      },
-      {
-        "title": "疯狂Spring Cloud连载（十一）——Feign的编码器与解码器",
-        "abstract": " 本文节选自《疯狂Spring Cloud微服务架构实战》，本书将于2017年11月出版。         Spring Cloud教学视频：",
-        "time": "杨小强 发布于 一年前",
-        "like": "122",
-        "commit": "101"
-      },
-      {
-        "title": "MySQL Schema与数据类型的优化",
-        "abstract": "选择优化的数据类型： 1、 更小的通常更好： 一般情况下，应该尽量使用可以正确存储数据的最小数据类型。",
-        "time": "杨小强 发布于 4小时前",
-        "like": "12",
-        "commit": "21"
-      },
-      {
-        "title": "MySQL Schema与数据类型的优化",
-        "abstract": "选择优化的数据类型： 1、 更小的通常更好： 一般情况下，应该尽量使用可以正确存储数据的最小数据类型。",
-        "time": "杨小强 发布于 4小时前",
-        "like": "12",
-        "commit": "21"
-      },
-      {
-        "title": "疯狂Spring Cloud连载（十一）——Feign的编码器与解码器",
-        "abstract": " 本文节选自《疯狂Spring Cloud微服务架构实战》，本书将于2017年11月出版。         Spring Cloud教学视频：",
-        "time": "杨小强 发布于 一年前",
-        "like": "122",
-        "commit": "101"
-      },
-      {
-        "title": "MySQL Schema与数据类型的优化",
-        "abstract": "选择优化的数据类型： 1、 更小的通常更好： 一般情况下，应该尽量使用可以正确存储数据的最小数据类型。",
-        "time": "杨小强 发布于 4小时前",
-        "like": "12",
-        "commit": "21"
+    let seq = this.api.get('pages').share();
+
+    seq.subscribe((res: any) => {
+      // If the API returned a successful response, mark the user as logged in
+      if (res.status == 'success') {
+        this.pages = res.data;
       }
-    ];
+    }, err => {
+      console.error('ERROR', err);
+    });
+
+    return seq;
   }
 
   //获取幻灯片
   getSlides() {
-    this.slideData = [{
-      "img": "http://images.csdn.net/20171023/u=1656509100,1818539814&fm=27&gp=0.jpg",
-      "title": "2017年含金量最高的机器学习技能或知识有哪些?"
-    }, {
-      "img": "http://images.csdn.net/20170704/1.png",
-      "title": "世界上第一个时序数据的Middle-Out算法压缩"
-    }, {
-      "img": "http://images.csdn.net/20171019/20171019084609390_%E5%89%AF%E6%9C%AC.jpg",
-      "title": "不能给祖国母亲添堵！技术人讲解十九大期间如何护航网站安全"
-    }, {
-      "img": "http://images.csdn.net/20171023/u=1656509100,1818539814&fm=27&gp=0.jpg",
-      "title": "2017年含金量最高的机器学习技能或知识有哪些?"
-    }, {
-      "img": "http://images.csdn.net/20170704/1.png",
-      "title": "世界上第一个时序数据的Middle-Out算法压缩"
-    }]
+    let seq = this.api.get('slides').share();
+
+    seq.subscribe((res: any) => {
+      // If the API returned a successful response, mark the user as logged in
+      if (res.status == 'success') {
+        this.pages = res.data;
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+
+    return seq;
+  }
+
+  goSearch(event){
+    console.log(event)
+    console.log("go search")
   }
 }
